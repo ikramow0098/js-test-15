b88dd4ca2b965d89e27cdd5d4e630f526219cf44
Fotihjon <ikromov@usf.edu>
2025-03-13 21:58:18 -0400

    Task 1 - Base Structure Setup.

--- cc_15.js
+++ cc_15.js
@@ -1,5 +1,11 @@
+//Task 1 - Base Structure Setup.
+
 document.addEventListener("DOMContentLoaded", function() {
   const riskDashboard = document.getElementById("riskDashboard");
   console.log("Risk Dashboard Loaded");
 }); 
+const riskForm = document.getElementById("riskForm");
+  const increaseRiskLevelsBtn = document.getElementById("increaseRiskLevels");
+
+
 
